db7b6c0036df99af451e473de565cc4c9c7a781c
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2016-01-12 23:21:10 -0300

    added the Products features

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,13 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+
    	add_to_products
   end
 
@@ -12,28 +15,80 @@ class Product
     @@products
   end
 
-  private
+  def self.find_by_title(title)
+    counter = 0
+  	Product.all.each do |product|
+	  	if product.title.eql?(title)
+	  		return product
+	  	end
+	  end
 
+    return nil
+  end
+  def self.product_include_by_title?(title)
+  	Product.all.each do |product|
+
+	  	if product.title.eql?(title)
+        return true 
+	  	else
+	  		return false
+	  	end
+	  
+    end	
+
+  end
+
+  #this method should return  an Array of 
+  #all products with a stock greater than zero.
+  def self.in_stock()
+      array_response = Array.new
+      Product.all.each do |product|
+
+        if product.in_stock?
+          array_response.push(product)
+        end
+      end 
+
+      if array_response.count>0
+        return array_response
+      else
+        puts "We have no Stock of toys!!"
+        return nil
+      end
+  end
+
+  #this method should return if there is stock of the toy
+  def in_stock?()
+    if self.stock >0
+      return true
+    else
+     return false
+    end
+  end
+
+  private
   #Method that provides the functionality to add a product into the 
   #Products class variable array
-  def add_to_products
-  	#we validate that the @@products has at least 1 prod 
-    #if not we add the without the duplicate validation
-    
+  def add_to_products()
+  	#we validate if we have products in the array, if so we check if the product
+  	#that we want to add is already there
     if @@products.count >0
-	  	Product.all.each do |product|
-	  		if product.title.eql?(self.title)  			
-	    		raise DuplicateProductError, "LEGO Iron Man vs. Ultron already exists. "
-	    	else	
-	    		puts product.title
-	   			@@products << self
-			end
 
-	  	end	
-	else
-    	@@products<<self
+    	if Product.product_include_by_title?(self.title)
+        
+
+    		raise DuplicateProductError, "#{self.title} already exists."
+    	else
+
+    		@@products.push(self)
+
+		  end
+	  else
+      @@products.push(self)
+    	
     end
-
-   
+ 
   end
+
+
 end
